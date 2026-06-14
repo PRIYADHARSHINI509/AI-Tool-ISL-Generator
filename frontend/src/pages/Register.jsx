e183e500ff0449d5986d97360db298a9561c1f2e
Priyadharshini <priyadharshinikamatchi66@gmail.com>
2026-06-14 12:55:22 +0530

    June 14 Login Page Development Completed

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,22 @@
+function Register() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Indian Sign Language Generator</h1>
+
+      <h2>Register</h2>
+
+      <input type="text" placeholder="Enter Name" />
+      <br /><br />
+
+      <input type="email" placeholder="Enter Email" />
+      <br /><br />
+
+      <input type="password" placeholder="Enter Password" />
+      <br /><br />
+
+      <button>Register</button>
+    </div>
+  );
+}
+
+export default Register;
